573a662c4f3383fb1aa1dfca1ecf5188a66d54e7
shubham <shubhshri25gmail.com>
2024-10-27 14:53:33 +0530

    email setup done

--- lib/dbConnect.ts
+++ lib/dbConnect.ts
@@ -1,18 +1,20 @@
 import mongoose from "mongoose";
 
-type connectionObject = {
+type ConnectionObject = {
   isConnected?: number;
 };
 
-const connection: connectionObject = {};
+const connection: ConnectionObject = {};
 
 async function dbConnect(): Promise<void> {
+  // Check if we have a connection to the database or if it's currently connecting
   if (connection.isConnected) {
     console.log("Already connected to the database");
     return;
   }
+
   try {
-    const db = await mongoose.connect(process.env.MONGO_URI || "", {});
+    const db = await mongoose.connect(process.env.MONGODB_URI || "", {});
 
     connection.isConnected = db.connections[0].readyState;
 
